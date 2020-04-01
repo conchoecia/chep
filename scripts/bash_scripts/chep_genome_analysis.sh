5f4294d6e565d2494a6356f84c26cf7a72762a5a
Darrin Schultz <darrin.schultz91@gmail.com>
2020-04-01 14:15:05 -0700

    updated chep_genome_analysis launch script

--- scripts/bash_scripts/chep_genome_analysis.sh
+++ scripts/bash_scripts/chep_genome_analysis.sh
@@ -3,11 +3,27 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 SNAKEFILE="${SCRIPTPATH}/../scripts/snakemake_scripts/Snakefile_chep_genome_analysis"
 ARGSTRING=""
 
-#CONFIGFILE="${PWD}/config.yaml"
-CONFIGFILE="test"
+CONFIGFILE="${PWD}/config.yaml"
+
+if [ "$#" -eq 0 ]
+then
+   snakemake --snakefile "${SNAKEFILE}" --config help=help
+   exit 1
+fi
 
 INCONFIG=0
 while (( "$#" )); do
+  echo "$1"
+  if [ "$1" == "--help" ]
+  then
+      snakemake --snakefile "${SNAKEFILE}" --config help=help
+      exit 1
+  fi
+  if [ "$1" == "-h" ]
+  then
+      snakemake --snakefile "${SNAKEFILE}" --config help=help
+      exit 1
+  fi
   if [ "${INCONFIG}" -eq 0 ]
   then
     if [ "$1" == "--configfile" ]
